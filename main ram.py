d0558cf1e3c5d8c8b761aa749de178d3d23bd0f8
Hardik Gupta <hdksir@gmail.com>
2019-03-04 19:06:29 +0530

    Add files via upload

---
+++ main ram.py
@@ -0,0 +1,20 @@
+from Tkinter import *
+root=Tk()
+root.geometry('900x600')
+root.title("Ram marriage bureau")
+def theFunction(e=0):
+    root.destroy()
+    import ramram
+root.configure(background="light blue")
+Label(root, text="Welcome to Matrimonial spot", font=("algerian", 40), fg="white",bg="#ff2299" ,width=27).place(x=0,y=0)
+Label(root, text="Presented By: Hardik Gupta", font=("algerian", 14), fg="blue", bg="green").place(x=10,y=80)
+Label(root, text="Enroll No.: 161B084", font=("algerian", 15), fg="blue", bg="green").place(x=10,y=120)
+Label(root, text="Batch: B3", font=("algerian", 15), fg="blue", bg="green").place(x=10,y=160)
+Label(root, text="University: JUET, GUNA", font=("algerian", 15), fg="blue", bg="green").place(x=10,y=200)
+Label(root, text="Email: hdksir@gmail.com", font=("Comic Sans MS", 15), fg="blue", bg="green").place(x=10,y=240)
+Label(root, text="Mob.: +919575794355", font=("Comic Sans MS", 15), fg="blue", bg="green").place(x=10,y=280)
+a=PhotoImage (file="ram.GIF")
+l=Label(root, image=a)
+l.place (x=550, y=120)
+l.bind('<Motion>',theFunction)
+root.mainloop()
